7d2ff308387a783ddabd49b2ada1f8eb1871415d
latufla <lafufla.work@gmail.com>
2014-04-03 19:20:41 +0700

    behaviors factory

--- BLight/BehaviorsFactory.cpp
+++ BLight/BehaviorsFactory.cpp
@@ -2,13 +2,35 @@
 #include "UserControlBehavior.h"
 #include "MoveBehavior.h"
 #include "AttackBehavior.h"
+#include "AIControlBehavior.h"
+#include "ChargePackBehavior.h"
+#include "ChargerBehavior.h"
+#include "DeathBehavior.h"
+#include "SpawnerBehavior.h"
+
+bool BehaviorsFactory::mapped = false;
+map <string, BehaviorBase*(*)()> BehaviorsFactory::nameToBehavior;
 
 BehaviorBase* BehaviorsFactory::create(string name)
 {
-	if(name == "UserControlBehavior")
-		return new UserControlBehavior();
-	else if(name == "MoveBehavior")
-		return new MoveBehavior();
-	else if(name == "AttackBehavior")
-		return new AttackBehavior();	
+	if(!mapped)
+		initMap();
+
+	return nameToBehavior[name]();
 }
+
+void BehaviorsFactory::initMap()
+{
+	nameToBehavior["UserControlBehavior"] = &createInstance<UserControlBehavior>;
+	nameToBehavior["AIControlBehavior"] = &createInstance<AIControlBehavior>;
+	
+	nameToBehavior["AttackBehavior"] = &createInstance<AttackBehavior>;
+	nameToBehavior["ChargePackBehavior"] = &createInstance<ChargePackBehavior>;
+	nameToBehavior["ChargerBehavior"] = &createInstance<ChargerBehavior>;
+	nameToBehavior["DeathBehavior"] = &createInstance<DeathBehavior>;
+	nameToBehavior["MoveBehavior"] = &createInstance<MoveBehavior>;
+	nameToBehavior["SpawnerBehavior"] = &createInstance<SpawnerBehavior>;
+
+	mapped = true;
+}
+
--- BLight/BehaviorsFactory.h
+++ BLight/BehaviorsFactory.h
@@ -1,9 +1,17 @@
 #pragma once
 #include "BehaviorBase.h"
 
+template<typename T> BehaviorBase* createInstance() { return new T; }
+
 class BehaviorsFactory
 {
 public:
 	static BehaviorBase* create(string);
+
+private:
+	static void initMap();
+	
+	static map <string,  BehaviorBase*(*)()> nameToBehavior;
+	static bool mapped;
 };
 
