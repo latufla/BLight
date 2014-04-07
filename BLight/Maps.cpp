fe0603146e210adf95636b5a1c4e4a1f2fe44ea3
latufla <lafufla.work@gmail.com>
2014-04-08 01:40:59 +0700

    all game infos in json

--- BLight/Maps.cpp
+++ BLight/Maps.cpp
@@ -16,18 +16,18 @@ vector<ControllerBase*> Maps::createDemoMap(FieldController& field)
 	ControllerBase* aiSpawner = field.createObjectController(2, *aiSpawnerInfo, CustomPoint(40.0f, 10.0f));
 	res.push_back(aiSpawner);
 
-// 	
-// 	ObjectInfo* chargerInfo = Infos::getInfoBy(Infos::CHARGER);
-// 	ControllerBase* charger = field.createObjectController(3, *chargerInfo, CustomPoint(5.0f, 20.0f));
-// 	res.push_back(charger);
-// 
-// 	ObjectInfo* energyPackInfo = Infos::getInfoBy(Infos::SMALL_ENERGY_PACK);
-// 	ControllerBase* energyPack = field.createObjectController(4, *energyPackInfo, CustomPoint(20.0f, 30.0f));
-// 	res.push_back(energyPack);
-// 
-// 	energyPackInfo = Infos::getInfoBy(Infos::MEDIUM_ENERGY_PACK);
-// 	energyPack = field.createObjectController(5, *energyPackInfo, CustomPoint(40.0f, 30.0f));
-// 	res.push_back(energyPack);
+	
+	ObjectInfo* chargerInfo = Infos::getInfoBy("charger");
+	ControllerBase* charger = field.createObjectController(3, *chargerInfo, CustomPoint(5.0f, 20.0f));
+	res.push_back(charger);
+
+	ObjectInfo* energyPackInfo = Infos::getInfoBy("small_energy_pack");
+	ControllerBase* energyPack = field.createObjectController(4, *energyPackInfo, CustomPoint(20.0f, 30.0f));
+	res.push_back(energyPack);
+
+	energyPackInfo = Infos::getInfoBy("medium_energy_pack");
+	energyPack = field.createObjectController(5, *energyPackInfo, CustomPoint(40.0f, 30.0f));
+	res.push_back(energyPack);
 	
 	return res;
 }
